cf50b8ba767ae39114776776e40b7b17178652ac
Enzu83 <enzu.mc@gmail.com>
2025-12-22 05:16:55 +0100

    feat: opcode 7 implemented

--- src/exec.rs
+++ src/exec.rs
@@ -1,41 +1,41 @@
 use std::process::exit;
 use std::{error::Error, fs::read};
 
-use crate::num::from_hex_slice;
+use crate::num::Number;
 use crate::instr::INSTRUCTION;
 
 pub struct Executable {
-    data: Vec<u16>,
+    data: Vec<Number>,
     ptr: usize,
 }
 
 impl Executable {
     pub fn new(path: &str) -> Result<Self, Box<dyn Error>> {
         let raw = read(path)?;
-        let data = from_hex_slice(&raw)?;
+        let data = Number::from_hex_slice(&raw)?;
 
         Ok(Self { data, ptr: 0 })
     }
 
-    fn next_number(&mut self) -> Result<u16, Box<dyn Error>> {
+    fn next_number(&mut self) -> Result<Number, Box<dyn Error>> {
         let number = self.data.get(self.ptr);
 
         self.ptr += 1;
 
         match number {
-            Some(value) => Ok(*value),
+            Some(num) => Ok(num.clone()),
             None => Err(format!("Attempt to read out of the executable code.").into()),
         }
     }
 
-    fn next_two_numbers(&mut self) -> Result<(u16, u16), Box<dyn Error>> {
+    fn next_two_numbers(&mut self) -> Result<(Number, Number), Box<dyn Error>> {
         let a = self.next_number()?;
         let b = self.next_number()?;
 
         Ok((a, b))
     }
 
-    fn next_three_numbers(&mut self) -> Result<(u16, u16, u16), Box<dyn Error>> {
+    fn next_three_numbers(&mut self) -> Result<(Number, Number, Number), Box<dyn Error>> {
         let a = self.next_number()?;
         let b = self.next_number()?;
         let c = self.next_number()?;
@@ -51,11 +51,15 @@ impl Executable {
     }
 
     fn next_instruction(&mut self) -> Result<INSTRUCTION, Box<dyn Error>> {
-        let instruction = match self.next_number()? {
+        let instruction = match self.next_number()?.value() {
             0 => INSTRUCTION::HALT,
             6 => {
                 let a = self.next_number()?;
                 INSTRUCTION::JMP(a)
+            },
+            7 => {
+                let (a, b) = self.next_two_numbers()?;
+                INSTRUCTION::JT(a, b)
             }
             19 => {
                 let a = self.next_number()?;
@@ -74,10 +78,15 @@ impl Executable {
                 exit(0);
             },
             INSTRUCTION::JMP(a) => {
-                self.ptr = a as usize;
+                self.ptr = a.value() as usize;
+            },
+            INSTRUCTION::JT(a, b) => {
+                if a.value() != 0 {
+                    self.ptr = b.value() as usize;
+                }
             }
             INSTRUCTION::OUT(a) => {
-                print!("{}", a as u8 as char);
+                print!("{}", a.value() as u8 as char);
             },
             INSTRUCTION::NOOP => {},
             _ => {},
--- src/num.rs
+++ src/num.rs
@@ -1,29 +1,54 @@
 use std::error::Error;
 
-pub fn from_hex_slice(raw: &[u8]) -> Result<Vec<u16>, Box<dyn Error>> {
-    if raw.len() % 2 != 0 {
-        return Err(format!("Size of hex slice should a multiple of 2. Got {}.", raw.len()).into());
-    }
-
-    let mut num_slice = Vec::with_capacity(raw.len() / 2);
-
-    for i in 0..raw.len() / 2 {
-        let num = from_hex(&raw[2*i..2*i+2])?;
-        num_slice.push(num);
-    }
-
-    Ok(num_slice)
+#[derive(Clone)]
+pub struct Number {
+    value: u16,
 }
 
-pub fn from_hex(raw: &[u8]) -> Result<u16, Box<dyn Error>> {
-    if raw.len() != 2 {
-        return Err(format!("Size of hex slice should be 2. Got {}.", raw.len()).into());
+impl Number {
+    pub fn from_hex_slice(raw: &[u8]) -> Result<Vec<Self>, Box<dyn Error>> {
+        if raw.len() % 2 != 0 {
+            return Err(format!("Size of hex slice should a multiple of 2. Got {}.", raw.len()).into());
+        }
+
+        let mut num_slice = Vec::with_capacity(raw.len() / 2);
+
+        for i in 0..raw.len() / 2 {
+            let num = Number::from_hex(&raw[2*i..2*i+2])?;
+            num_slice.push(num);
+        }
+
+        Ok(num_slice)
     }
 
-    let hex = format!("{:02X}{:02X}", raw[1], raw[0]);
-    let value = u16::from_str_radix(&hex, 16)?;
+    pub fn from_hex(raw: &[u8]) -> Result<Self, Box<dyn Error>> {
+        if raw.len() != 2 {
+            return Err(format!("Size of hex slice should be 2. Got {}.", raw.len()).into());
+        }
 
-    Ok(value)
+        let hex = format!("{:02X}{:02X}", raw[1], raw[0]);
+        let value = u16::from_str_radix(&hex, 16)?;
+
+        Ok(Self { value })
+    }
+
+    pub fn value(&self) -> u16 {
+        self.value
+    }
+
+    pub fn assert_literal(&self) -> Result<(), Box<dyn Error>> {
+        if self.value > 32767 {
+            return Err(format!("{} is not a literal value", self.value).into());
+        }
+
+        Ok(())
+    }
+
+    pub fn assert_register(&self) -> Result<(), Box<dyn Error>> {
+        if self.value <= 32767 || self.value > 32775 {
+            return Err(format!("{} is not a register", self.value).into());
+        }
+        
+        Ok(())
+    }
 }
-
-
